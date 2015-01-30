4d77b6cacc3700cf0dd4ed40274f2519d6adadfe
Neeten <sandra.eliasson94@gmail.com>
2015-01-30 11:59:42 +0100

    hej

--- src/se/dreamteam/atm/exception/ATMException.java
+++ src/se/dreamteam/atm/exception/ATMException.java
@@ -2,10 +2,15 @@ package se.dreamteam.atm.exception;
 
 public class ATMException extends RuntimeException
 {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 9187187163143195697L;
 
+	public ATMException(String message, Throwable clause)
+	{
+		super(message, clause);
+	}
+
+	public ATMException(String message)
+	{
+		super(message);
+	}
 }
--- src/se/dreamteam/atm/model/ATMCard.java
+++ src/se/dreamteam/atm/model/ATMCard.java
@@ -2,5 +2,29 @@ package se.dreamteam.atm.model;
 
 public class ATMCard
 {
+	private final String accountHolderId;
+	private final String bankId;
+	private final int pin;
 
+	public ATMCard(String accountHolderId, String bankId, int pin)
+	{
+		this.accountHolderId = accountHolderId;
+		this.bankId = bankId;
+		this.pin = pin;
+	}
+
+	public String getAccountHolderId()
+	{
+		return accountHolderId;
+	}
+
+	public String getBankId()
+	{
+		return bankId;
+	}
+
+	public boolean verifyPin(int pin)
+	{
+		return this.pin == pin;
+	}
 }
