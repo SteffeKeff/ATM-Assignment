2984f91e8a366aaa9e727ae329e7f80801c48aba
Stefan De Geer <sdgkeff@gmail.com>
2015-01-30 11:40:27 +0100

    fixat errors hihi

--- src/se/dreamteam/atm/service/AbstractATMSession.java
+++ src/se/dreamteam/atm/service/AbstractATMSession.java
@@ -1,10 +1,6 @@
 package se.dreamteam.atm.service;
 
-<<<<<<< HEAD
-public class AbstractATMSession
-=======
 public abstract class AbstractATMSession
->>>>>>> 9b64da43b425ee020cede356742d2f40c631dc6a
 {
 
 }
