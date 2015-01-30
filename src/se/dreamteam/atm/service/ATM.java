1244193bab20fb554f58624b75b61641b98e6559
Stefan De Geer <sdgkeff@gmail.com>
2015-01-30 14:01:31 +0100

    Börjat med uppgifterna och implementerat lite i ATM

--- src/se/dreamteam/atm/service/ATM.java
+++ src/se/dreamteam/atm/service/ATM.java
@@ -3,22 +3,35 @@ package se.dreamteam.atm.service;
 import java.util.List;
 import java.util.Map;
 
+import se.dreamteam.atm.exception.ATMException;
+import se.dreamteam.atm.exception.ATMSecurityException;
+import se.dreamteam.atm.model.ATMCard;
+
 public final class ATM
 {
 	private final Map<String, Bank> banks;
-	
+
 	public ATM(List<Bank> banks)
 	{
-		//To be implemented
+		// To be implemented
 	}
-	
+
 	public ATMSession verifyPin(int pin, ATMCard card)
 	{
-		//To be implemented
+		if (card.verifyPin(pin))
+		{
+			return new ATMSessionImpl(card, getBank(card));
+		}
+		throw new ATMSecurityException("Invalid pincode");
 	}
-	
-	private Bank getBank(ATMCard card){
-		//To be implemented
+
+	private Bank getBank(ATMCard card)
+	{
+		if (banks.containsKey(card.getBankId()))
+		{
+			return banks.get(card.getBankId());
+		}
+		throw new ATMException("Bank does not exist");
 	}
-	
+
 }
--- src/se/dreamteam/atm/service/ATMSessionImpl.java
+++ src/se/dreamteam/atm/service/ATMSessionImpl.java
@@ -1,6 +1,36 @@
 package se.dreamteam.atm.service;
 
-public class ATMSessionImpl
+import se.dreamteam.atm.exception.ATMException;
+import se.dreamteam.atm.model.ATMCard;
+import se.dreamteam.atm.model.ATMReceipt;
+
+public class ATMSessionImpl extends AbstractATMSession
 {
+
+	public ATMSessionImpl(ATMCard atmCard, Bank bank)
+	{
+		super(atmCard, bank);
+	}
+
+	@Override
+	public long withdrawAmount(int amount)
+	{
+		if(amount < 100 || amount > 10000 || amount%100 != 0){
+			throw new ATMException("The amount is not valid");
+		}
+		return 0; //Ska returnera saldo
+	}
+
+	@Override
+	public ATMReceipt requestReceipt(long transactionId)
+	{
+		return null;
+	}
+
+	@Override
+	public long checkBalance()
+	{
+		return 0;
+	}
 
 }
