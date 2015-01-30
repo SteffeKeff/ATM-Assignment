e5a91deff4d0dfe2d11f44584c31821493a45114
Stefan De Geer <sdgkeff@gmail.com>
2015-01-30 13:19:26 +0100

    lagt in Secut lagt in Security Exception

--- src/se/dreamteam/atm/exception/ATMSecurityException.java
+++ src/se/dreamteam/atm/exception/ATMSecurityException.java
@@ -1,6 +1,17 @@
 package se.dreamteam.atm.exception;
 
-public class ATMSecurityException
+public class ATMSecurityException extends RuntimeException
 {
+	private static final long serialVersionUID = -3031452609540350355L;
+	
+	public ATMSecurityException(String message, Throwable clause)
+	{
+		super(message, clause);
+	}
 
+	public ATMSecurityException(String message)
+	{
+		super(message);
+	}
+	
 }
